573ec1c73f6622c3dd4d3ece227dc0757dbbfaff
thenaijagamer <sabdullahialaba50@gmail.com>
2025-07-21 21:17:54 +0100

    Fix more build error

--- src/api/course/section/queries.ts
+++ src/api/course/section/queries.ts
@@ -80,7 +80,7 @@ export interface CourseSectionQuiz {
   title: string;
   description: string;
   questions: CourseSectionQuizQuestion[];
-  isContentCompleted: boolean;
+  isContentCompleted?: boolean;
 }
 
 export interface CourseSectionQuizQuestion {
