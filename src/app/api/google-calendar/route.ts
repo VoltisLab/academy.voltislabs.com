be64d73f2f6af9dbcf231e3202adc012e8409267
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2025-07-21 21:11:15 +0100

    fix conflit

--- src/app/api/google-calendar/route.ts
+++ src/app/api/google-calendar/route.ts
@@ -1,7 +1,7 @@
 // app/api/calendar/route.ts (or wherever your calendar route is)
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../auth/refreshable/[...nextauth]/routes";
 
 export async function POST(req: NextRequest) {
   try {
